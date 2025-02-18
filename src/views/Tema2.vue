e8966519ed47cfb22a85f69aeb89c6c8b036da35
AdonadoMolinares <donadomolinares@gmail.com>
2025-02-18 17:56:55 -0500

    ac

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,16 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Importancia de las frutas y verduras
+    
+  
+    .row.mb-5
+      .col-lg-4.mb-3.mb-lg-0
+        img(src="../assets/curso/temas/tema2/imgtema2.png", alt="Imagen alineada izquierda" class="img-fluid")
+      .col-lg-8.
+        p Las frutas y verduras son fundamentales en nuestra dieta y tienen un potente impacto en la salud humana, gracias a su contenido diverso y rico en compuestos bioactivos. Estas sustancias, conocidas como fitoquímicos, están presentes en los alimentos de origen vegetal e intervienen en múltiples procesos biológicos, ofreciendo beneficios que van más allá de la nutrición básica. A través de la investigación, se ha demostrado que estos compuestos pueden contribuir a la prevención de enfermedades, mejorar la salud cardiovascular y retrasar el envejecimiento. En este aparte, exploraremos las sustancias bioactivas, los esteroles vegetales y los mecanismos que afectan la calidad de las frutas y verduras, resaltando su papel crucial en la promoción del bienestar y la salud a lo largo de la vida. 
+    
+    
 
 </template>
 
